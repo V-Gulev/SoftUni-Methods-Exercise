eea827c73dfaa0172923ded37d0c86567de3c383
V-Gulev <velizargulev@abv.bg>
2024-08-17 12:28:32 +0300

    Greater of Two Values

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,48 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.parseInt(scanner.nextLine());
-        int power = Integer.parseInt(scanner.nextLine());
+        String type = scanner.nextLine();
 
-        System.out.printf("%.0f",mathPower(number,power));
-
-    }
-
-    public static double mathPower(int number, int power) {
-        double result = 1;
-        if (power>1){
-            result= Math.pow(number,power);
+        switch (type){
+            case "int":
+                int num1 = Integer.parseInt(scanner.nextLine());
+                int num2 = Integer.parseInt(scanner.nextLine());
+                System.out.println(GetMax(num1,num2));
+                break;
+            case "char":
+                char firstChar = scanner.nextLine().charAt(0);
+                char secondChar = scanner.nextLine().charAt(0);
+                System.out.println(GetMax(firstChar,secondChar));
+                break;
+            case "string":
+                String firstString = scanner.nextLine();
+                String secondString =scanner.nextLine();
+                System.out.println(GetMax(firstString,secondString));
+                break;
         }
-        return result;
+
+
     }
+
+    public static int GetMax(int num1, int num2) {
+        return Math.max(num1, num2);
+    }
+
+    public static char GetMax(char first, char second) {
+        if (first > second) {
+            return first;
+        }
+        return second;
+    }
+
+    public static String GetMax(String first, String second) {
+        if (first.compareTo(second)>=0) {
+            return first;
+        }
+        return second;
+    }
+
+
+
 
 }
