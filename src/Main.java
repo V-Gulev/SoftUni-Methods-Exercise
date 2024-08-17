4ea1ebf2fcca54410aef047c82e53257907afc42
V-Gulev <velizargulev@abv.bg>
2024-08-17 12:34:53 +0300

    Password Validator

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String char1 = scanner.next();
-        String char2 = scanner.next();
-        PrintCharactersInRange(char1, char2);
+        String password = scanner.nextLine();
+        if (PasswordLengthValidator(password) && PasswordDigitValidator(password) && PasswordSymbolsValidator(password)){
+            System.out.println("Password is valid");
+        }else {
+            if (!PasswordLengthValidator(password)){
+                System.out.println("Password must be between 6 and 10 characters");
+            }
+            if (!PasswordSymbolsValidator(password)){
+                System.out.println("Password must consist only of letters and digits");
+            }
+            if (!PasswordDigitValidator(password)){
+                System.out.println("Password must have at least 2 digits");
+            }
+        }
+
+
     }
 
-    public static void PrintCharactersInRange(String char1, String char2) {
-        char start = char1.charAt(0);
-        char end = char2.charAt(0);
-        if ((int) start > (int) end){
-            char temp = start;
-            start=end;
-            end=temp;
+    public static boolean PasswordLengthValidator(String password) {
+        return password.length() >= 6 && password.length() <= 10;
+    }
+    public static boolean PasswordDigitValidator(String password) {
+        boolean validPassword = true;
+        int digitCounter = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))){
+                digitCounter++;
+            }
         }
-        for (int i = (int) start + 1; i < (int) end ; i++) {
-            System.out.print((char) i + " ");
+        if (digitCounter < 2){
+            validPassword = false;
         }
+        return validPassword;
+    }
+
+    public static boolean PasswordSymbolsValidator(String password) {
+        boolean validPassword = true;
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isDigit(password.charAt(i)) && !Character.isAlphabetic(password.charAt(i))){
+                validPassword = false;
+            }
+        }
+        return validPassword;
     }
 
 }
