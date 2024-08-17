37a3ae9bd903ca1b4152167843816722870aa073
V-Gulev <velizargulev@abv.bg>
2024-08-17 12:32:54 +0300

    Smallest of Three Numbers

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = Integer.parseInt(scanner.nextLine());
-        String operator = scanner.nextLine();
         int num2 = Integer.parseInt(scanner.nextLine());
-        System.out.println(Calculator(num1,operator,num2));
+        int num3 = Integer.parseInt(scanner.nextLine());
+        PrintSmallestNumber(num1, num2, num3);
     }
 
-    public static int Calculator(int num1,String operator,int num2) {
-
-        return switch (operator) {
-            case "/" -> num1 / num2;
-            case "*" -> num1 * num2;
-            case "+" -> num1 + num2;
-            case "-" -> num1 - num2;
-            default -> 0;
-        };
+    public static void PrintSmallestNumber(int num1, int num2, int num3) {
+        int smallestNumber = num1;
+        if (smallestNumber > num2) {
+            smallestNumber = num2;
+        }
+        if (smallestNumber > num3) {
+            smallestNumber = num3;
+        }
+        System.out.println(smallestNumber);
     }
 
 }
