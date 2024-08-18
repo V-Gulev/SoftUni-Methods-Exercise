66c1bec1343c8c5b07efc67b88fb6f3e53e788cd
V-Gulev <velizargulev@abv.bg>
2024-08-18 15:47:36 +0300

    Factorial Division

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = Integer.parseInt(scanner.nextLine());
-        PrintNxM(num);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        PrintDividedFactorials(num1, num2);
     }
 
-    public static void PrintNxM(int num) {
-        for (int i = 0; i < num; i++) {
-            for (int j = 1; j <= num; j++) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
+    public static void PrintDividedFactorials(int num1, int num2) {
+        double fact1 = 1;
+        double fact2 = 1;
+        double divided;
+        for (int i = 1; i <= num1; i++) {
+            fact1 *= i;
         }
+        for (int i = 1; i <= num2; i++) {
+            fact2 *= i;
+        }
+        divided = fact1 / fact2;
+        System.out.printf("%.2f", divided);
+
+
     }
-
-
 }
+
+
+
 
 
 
