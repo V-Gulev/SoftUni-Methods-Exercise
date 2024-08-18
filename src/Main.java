5932f5033ec31a9493ba35c9e25e85b0b9864a4e
V-Gulev <velizargulev@abv.bg>
2024-08-18 15:50:58 +0300

    Data Types

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int end = Integer.parseInt(scanner.nextLine());
-        PrintTopNumbers(end);
-    }
-
-    public static void PrintTopNumbers(int end) {
-        int sumOfDigits = 0;
-        int oddDigitsCounter = 0;
-
-        for (int i = 0; i <= end; i++) {
-
-            int number = i;
-            while (number > 0) {
-                int lastDigit = number % 10;
-                number /= 10;
-                sumOfDigits += lastDigit;
-                if (lastDigit % 2 != 0) {
-                    oddDigitsCounter++;
-                }
-
-            }
-
-            if (oddDigitsCounter > 0 && sumOfDigits % 8 == 0) {
-                System.out.println(i);
-            }
-
-            sumOfDigits = 0;
-            oddDigitsCounter = 0;
+        String dataType = scanner.nextLine();
+        switch (dataType) {
+            case "int":
+                int int1 = Integer.parseInt(scanner.nextLine());
+                System.out.println(int1 * 2);
+                break;
+            case "real":
+                double double1 = Double.parseDouble(scanner.nextLine()) * 1.50;
+                System.out.printf("%.2f", double1);
+                break;
+            case "string":
+                String input = scanner.nextLine();
+                System.out.println('$' + input + '$');
+                break;
 
         }
-
     }
-
 }
-
-
 
 
 
