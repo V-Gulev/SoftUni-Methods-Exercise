8a5689797fcedc95dcaa683f47c30cbe5ea6886c
V-Gulev <velizargulev@abv.bg>
2024-08-18 15:48:43 +0300

    Palindrome Integers

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num1 = Integer.parseInt(scanner.nextLine());
-        int num2 = Integer.parseInt(scanner.nextLine());
-        PrintDividedFactorials(num1, num2);
+        PalindromeNumbers();
     }
 
-    public static void PrintDividedFactorials(int num1, int num2) {
-        double fact1 = 1;
-        double fact2 = 1;
-        double divided;
-        for (int i = 1; i <= num1; i++) {
-            fact1 *= i;
-        }
-        for (int i = 1; i <= num2; i++) {
-            fact2 *= i;
-        }
-        divided = fact1 / fact2;
-        System.out.printf("%.2f", divided);
+    public static void PalindromeNumbers() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("END")) {
+                break;
+            }
+            String duplicate = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                duplicate += Character.toString(input.charAt(i));
+            }
 
+            if (duplicate.equals(input)) {
+                System.out.println("true");
+            } else System.out.println("false");
+        }
 
     }
 }
