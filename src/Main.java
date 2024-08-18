5f9907c0302f0dd3bac6fc50175983f8e76316d8
V-Gulev <velizargulev@abv.bg>
2024-08-18 15:49:42 +0300

    Top Number

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        PalindromeNumbers();
+        Scanner scanner = new Scanner(System.in);
+        int end = Integer.parseInt(scanner.nextLine());
+        PrintTopNumbers(end);
     }
 
-    public static void PalindromeNumbers() {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String input = sc.nextLine();
-            if (input.equals("END")) {
-                break;
-            }
-            String duplicate = "";
-            for (int i = input.length() - 1; i >= 0; i--) {
-                duplicate += Character.toString(input.charAt(i));
+    public static void PrintTopNumbers(int end) {
+        int sumOfDigits = 0;
+        int oddDigitsCounter = 0;
+
+        for (int i = 0; i <= end; i++) {
+
+            int number = i;
+            while (number > 0) {
+                int lastDigit = number % 10;
+                number /= 10;
+                sumOfDigits += lastDigit;
+                if (lastDigit % 2 != 0) {
+                    oddDigitsCounter++;
+                }
+
             }
 
-            if (duplicate.equals(input)) {
-                System.out.println("true");
-            } else System.out.println("false");
+            if (oddDigitsCounter > 0 && sumOfDigits % 8 == 0) {
+                System.out.println(i);
+            }
+
+            sumOfDigits = 0;
+            oddDigitsCounter = 0;
+
         }
 
     }
+
 }
 
 
